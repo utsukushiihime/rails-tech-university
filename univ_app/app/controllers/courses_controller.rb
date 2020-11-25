aec6aa02bc65048f853b4b411467fc47c3fec179
creattech <crystalsmcneil@gmail.com>
2020-11-24 22:01:24 -0600

    adds links to routes

---
+++ univ_app/app/controllers/courses_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+class CoursesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+  
+end
--- univ_app/config/routes.rb
+++ univ_app/config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root 'courses#index'
+  get 'courses/new', to: 'courses#new'
+  get 'about', to: 'pages#about'
 end
