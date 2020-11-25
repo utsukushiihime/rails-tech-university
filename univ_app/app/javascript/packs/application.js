94f4d0e5a7ea1a91b0ce92010a8635167d62750b
creattech <crystalsmcneil@gmail.com>
2020-11-25 14:17:35 -0600

    adds nav

--- univ_app/app/javascript/packs/application.js
+++ univ_app/app/javascript/packs/application.js
@@ -4,13 +4,13 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+import "@fortawesome/fontawesome-free/js/all";
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
 
 Rails.start()
 Turbolinks.start()
