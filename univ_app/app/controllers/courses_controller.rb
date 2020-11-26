88acce4d38b327ad4b1e19ed3d6e2a82788b0034
creattech <crystalsmcneil@gmail.com>
2020-11-26 12:50:46 -0600

    adds student routes

--- univ_app/app/controllers/courses_controller.rb
+++ univ_app/app/controllers/courses_controller.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
-class CoursesController < ApplicationController
 
+class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
 
-  def new
-
-  end
-  
+  def new; end
 end
--- univ_app/app/controllers/students_controller.rb
+++ univ_app/app/controllers/students_controller.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class StudentsController < ApplicationController
   def index
+    @students = Student.all
+  end
 
+  def new
+    @student = Student.new
   end
 end
