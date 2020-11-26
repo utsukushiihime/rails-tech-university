69f86a5ec13a9f4303afcee20b35cb8e67a49534
creattech <crystalsmcneil@gmail.com>
2020-11-25 23:55:31 -0600

    adds student model

--- univ_app/db/schema.rb
+++ univ_app/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_235522) do
+ActiveRecord::Schema.define(version: 2020_11_26_051408) do
 
   create_table "courses", force: :cascade do |t|
     t.string "short_name"
     t.string "name"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
